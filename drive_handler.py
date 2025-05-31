1c595a8a4810670f53d0ee64263ad3a70d54a14a
WatabooColosus <168364521+WatabooColosus@users.noreply.github.com>
2025-05-31 00:35:03 +0000

    4543543534534532323

--- drive_handler.py
+++ drive_handler.py
@@ -1,28 +1,26 @@
 import os
-import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
+# Ruta a las credenciales (Render debe montarlas como secret)
 GOOGLE_CREDENTIALS_PATH = "/etc/secrets/ci-credenciales.json"
 DRIVE_FOLDER_ID = "16K22oTxfXYTUjYqzcRqtuMWtZ39V6xEc"
 
-with open(GOOGLE_CREDENTIALS_PATH, "r") as f:
-    creds_dict = json.load(f)
-
-credentials = service_account.Credentials.from_service_account_info(
-    creds_dict,
-    scopes=["https://www.googleapis.com/auth/drive.file"],
+credentials = service_account.Credentials.from_service_account_file(
+    GOOGLE_CREDENTIALS_PATH,
+    scopes=["https://www.googleapis.com/auth/drive"]
 )
-
 drive_service = build("drive", "v3", credentials=credentials)
 
-def upload_file_to_drive(file_obj):
+def upload_file_to_drive(temp_file):
     file_metadata = {
-        "name": file_obj.filename,
-        "parents": [DRIVE_FOLDER_ID],
+        "name": temp_file.name,
+        "parents": [DRIVE_FOLDER_ID]
     }
-    media = MediaFileUpload(file_obj.filepath)
+
+    media = MediaFileUpload(temp_file.path, mimetype="text/plain")
+
     uploaded_file = drive_service.files().create(
         body=file_metadata,
         media_body=media,
